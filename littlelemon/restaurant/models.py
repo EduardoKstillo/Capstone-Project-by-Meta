0abcc9e32cc4aebf070e54be02977bf7cc611907
EduardoKstillo <eddy.castillo.dev@gmail.com>
2024-03-10 23:03:31 -0500

    booking model and menu created

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Booking (models.Model):
+    name = models.CharField(max_length=100)
+    no_of_guests = models.IntegerField(max_length=6)
+    bookingDate = models.DateTimeField(auto_now_add=True)
+
+
+class Menu (models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField()
+    inventory = models.IntegerField(max_length=5)
